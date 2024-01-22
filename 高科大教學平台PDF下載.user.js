ab7b53cd2cc76f6195a6bddd91a77ab8b293afd3
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2024-01-22 14:38:11 +0800

    Add files via upload

---
+++ 高科大教學平台PDF下載.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         高科大教學平台PDF下載
+// @namespace    https://facaikotei.github.io/
+// @version      1
+// @description  高科大教學平台PDF下載
+// @author       facaikotei
+// @match        https://elearning.nkust.edu.tw/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.getElementById("download").style.display="inline-block";
+    document.getElementById("download").onclick=function(){window.open(DEFAULT_URL)};
+})();
