dbb7dcc7caaa4d33261c7b10801ab4ad6a851e25
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2024-05-24 02:23:55 +0800

    Update 高科大教學平台PDF下載.user.js

--- 高科大教學平台PDF下載.user.js
+++ 高科大教學平台PDF下載.user.js
@@ -2,7 +2,7 @@
 // @name         高科大教學平台PDF下載
 // @namespace    https://facaikotei.github.io/
 // @version      1.1
-// @description  強制啟用PDF下載按鈕
+// @description  強制啟用PDF下載功能
 // @author       facaikotei
 // @match        https://elearning.nkust.edu.tw/*
 // @icon         https://elearning.nkust.edu.tw/base/10001/door/tpl/icon.ico
