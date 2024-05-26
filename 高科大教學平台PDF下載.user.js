13e88dd6ae003c730a8f6bfa558c4687915b5b4f
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2024-05-26 12:50:18 +0800

    Update 高科大教學平台PDF下載.user.js

--- 高科大教學平台PDF下載.user.js
+++ 高科大教學平台PDF下載.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name				nkust elearning PDF download
-// @name:zh-TW			高科大教學平台PDF下載
-// @name:zh-CN			高科大教学平台PDF下载
-// @namespace			https://facaikotei.github.io/
-// @version				2
-// @description			Force enable PDF downloading feature
-// @description:zh-TW	強制啟用PDF下載功能
+// @name:en		nkust elearning PDF download
+// @name		高科大教學平台PDF下載
+// @name:zh-CN		高科大教学平台PDF下载
+// @namespace		https://facaikotei.github.io/
+// @version		2
+// @description:en	Force enable PDF downloading feature
+// @description		強制啟用PDF下載功能
 // @description:zh-CN	强制启用PDF下载功能
-// @match				https://elearning.nkust.edu.tw/learn/path/viewPDF.php
-// @icon				https://elearning.nkust.edu.tw/base/10001/door/tpl/icon.ico
-// @grant				none
-// @license				MIT
-// @homepageURL			https://facaikotei.github.io/
-// @downloadURL			https://github.com/facaikotei/nkust-pdf-dl/raw/main/%E9%AB%98%E7%A7%91%E5%A4%A7%E6%95%99%E5%AD%B8%E5%B9%B3%E5%8F%B0PDF%E4%B8%8B%E8%BC%89.user.js
-// @supportURL			https://github.com/facaikotei/nkust-pdf-dl/issues
+// @match		https://elearning.nkust.edu.tw/learn/path/viewPDF.php
+// @icon		https://elearning.nkust.edu.tw/base/10001/door/tpl/icon.ico
+// @grant		none
+// @license		MIT
+// @homepageURL		https://facaikotei.github.io/
+// @downloadURL		https://github.com/facaikotei/nkust-pdf-dl/raw/main/%E9%AB%98%E7%A7%91%E5%A4%A7%E6%95%99%E5%AD%B8%E5%B9%B3%E5%8F%B0PDF%E4%B8%8B%E8%BC%89.user.js
+// @supportURL		https://github.com/facaikotei/nkust-pdf-dl/issues
 // ==/UserScript==
 
 (function() {
