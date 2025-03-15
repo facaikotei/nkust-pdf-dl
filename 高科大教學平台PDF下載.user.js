c7218da34717111bf4bc8f346c36584a5b2ff0e1
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2025-03-15 22:27:33 +0800

    Update 高科大教學平台PDF下載.user.js

--- 高科大教學平台PDF下載.user.js
+++ 高科大教學平台PDF下載.user.js
@@ -1,18 +1,14 @@
 // ==UserScript==
 // @name		高科大教學平台PDF下載
-// @name:zh-CN		高科大教学平台PDF下载
-// @name:en		nkust elearning PDF download
 // @namespace		https://facaikotei.github.io/
 // @version		2
 // @description		強制啟用PDF下載功能
-// @description:zh-CN	强制启用PDF下载功能
-// @description:en	Force enable PDF downloading feature
 // @match		https://elearning.nkust.edu.tw/learn/path/viewPDF.php?*
 // @icon		https://elearning.nkust.edu.tw/base/10001/door/tpl/icon.ico
 // @grant		none
 // @license		MIT
-// @homepageURL		https://facaikotei.github.io/
-// @downloadURL		https://github.com/facaikotei/nkust-pdf-dl/raw/main/%E9%AB%98%E7%A7%91%E5%A4%A7%E6%95%99%E5%AD%B8%E5%B9%B3%E5%8F%B0PDF%E4%B8%8B%E8%BC%89.user.js
+// @homepageURL		https://greasyfork.org/zh-TW/scripts/496114-%E9%AB%98%E7%A7%91%E5%A4%A7%E6%95%99%E5%AD%B8%E5%B9%B3%E5%8F%B0pdf%E4%B8%8B%E8%BC%89
+// @downloadURL		https://update.greasyfork.org/scripts/496114/%E9%AB%98%E7%A7%91%E5%A4%A7%E6%95%99%E5%AD%B8%E5%B9%B3%E5%8F%B0PDF%E4%B8%8B%E8%BC%89.user.js
 // @supportURL		https://github.com/facaikotei/nkust-pdf-dl
 // ==/UserScript==
 
@@ -22,6 +18,6 @@
     if(document.getElementById("download").style.display=="none")
     {
         document.getElementById("download").style.display="block";
-        document.getElementById("download").onclick=function(){window.open(DEFAULT_URL.replaceAll("%2F","/"))};
+        document.getElementById("download").onclick=function(){window.open(DEFAULT_URL.replaceAll("%2F","/"));};
     }
 })();
